801ffd3f16a5eef813f885bed9718bc50255ad6f
Jeff Caldwell <nemo.omen@gmail.com>
2020-11-12 10:47:58 -0600

    Add tinro

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import { default as express } from 'express';
 import { default as logger } from 'morgan';
 import { default as bodyParser } from 'body-parser';
 import { default as cookieParser } from 'cookie-parser';
+import cors from 'cors';
 import * as http from 'http';
 import * as path from 'path';
-import { approotdir } from './helpers/approot.js';
+
+import { approotdir } from './approot.js';
 import { router as apiRouter } from './routes/api.js';
 
 const __dirname = approotdir;
@@ -12,8 +14,12 @@ const __dirname = approotdir;
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 
 app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 //we want that JSON data!
 app.use(bodyParser.json());
